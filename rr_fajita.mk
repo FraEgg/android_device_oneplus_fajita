e352ea0bddb62c133d6047a92347152888976088
Frank <eggen@emden.net>
2019-02-14 19:56:15 +0100

    Changes for RR

--- rr_fajita.mk
+++ rr_fajita.mk
@@ -22,7 +22,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/oneplus/fajita/device.mk)
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/rr/config/common_full_phone.mk)
 
 PRODUCT_NAME := rr_fajita
 PRODUCT_DEVICE := fajita
